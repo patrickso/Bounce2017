a8607bb31890d74724a86d0d38b725fe121a84a5
Patrick So <pso@email.wm.edu>
2017-01-20 18:44:49 -0500

    Can switch scenes

---
+++ BounceTheGame2017/Assets/navigation_script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navigation_script : MonoBehaviour {
+
+	// Use this for initialization
+	public void startPlay() {
+		SceneManager.LoadScene ("PlayScene");
+	}
+
+	public void startMain(){
+		SceneManager.LoadScene ("MainScene");
+	}
+}
