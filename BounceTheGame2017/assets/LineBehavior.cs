30bbf3634aa52d72030c8ff36c2677e9c69be9ed
John Wilkes <jcwilkes@email.wm.edu>
2017-02-07 13:49:11 -0500

    Working settings menu and line creation.

---
+++ BounceTheGame2017/assets/LineBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineBehavior : MonoBehaviour {
+	public GameObject line;
+
+	void OnCollisionEnter ( Collision collision){
+		Debug.Log ("Collided");
+		line.SetActive (false);
+	}
+}
